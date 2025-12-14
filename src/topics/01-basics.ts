27a30199a1bbbba6961f57f67dd8134b5f5f7ce9
Eduardo Sandoval <edsandoval.dev@gmail.com>
2025-12-13 23:09:11 -0600

    Basic variable types

---
+++ src/topics/01-basics.ts
@@ -0,0 +1,13 @@
+// Tipo texto
+const name: string = "Eduardo";
+
+// Tipo número o el valor exacto "FULL"
+let hpPoints: number | "FULL" = 95;
+hpPoints = "FULL";
+
+// Tipo booleano
+const isAlive: boolean = true;
+
+console.log({name, hpPoints, isAlive});
+
+export {};
