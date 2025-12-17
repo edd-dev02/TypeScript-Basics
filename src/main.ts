6963ca7afe82d343e6027985a6d3e30fc28dd1d1
Eduardo Sandoval <edsandoval.dev@gmail.com>
2025-12-17 14:48:40 -0600

    practice argument destructuring and typed returns in TypeScript

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,14 @@ import './style.css'
 // import "./topics/03-functions"
 //import "./topics/04-homework-types"
 //import "./topics/05-basic-destructuring"
+import "./topics/06-function-destructuring"
 
 //import "./exercises/excercise3"
 //import "./exercises/functions/excercise4"
 //import "./exercises/destructuring/exercise1"
 //import "./exercises/destructuring/exercise2"
 //import "./exercises/destructuring/exercise3"
-import "./exercises/destructuring/exercise4"
+//import "./exercises/destructuring/exercise4"
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <p>Hola mundo</p>
---
+++ src/topics/06-function-destructuring.ts
@@ -0,0 +1,49 @@
+
+interface Product {
+    description: string;
+    price: number;
+}
+
+const phone: Product = {
+    description: "Nokia A1",
+    price: 150.0
+}
+
+const tablet: Product = {
+    description: "iPad Air",
+    price: 250.0
+}
+
+interface TaxCalculationOptions {
+    tax: number;
+    products: Product[];
+}
+
+//function taxCalculation ( options: TaxCalculationOptions ): [number, number] {
+function taxCalculation ( {tax, products}: TaxCalculationOptions ): [number, number] {
+
+    // const { tax, products } = options // Usar esta desestructuración en caso de que el objeto tenga muchas llaves
+    
+    let total = 0;
+    
+    products.forEach( ( { price } ) => {
+        total += price;
+    });
+
+    return [total, total * tax];
+
+}
+
+const shoppingCart: Product[] = [phone, tablet];
+const tax = 0.15;
+
+const options: TaxCalculationOptions = {
+    tax,
+    products: shoppingCart
+};
+
+const [total, impuestos] = taxCalculation( options );
+
+console.log(`Total de la venta: ${total} - Impuesto sobre las ventas: ${impuestos}`);
+
+export {};
