be5be2328f1fc7f220138ce389fc7830aed69d61
Eduardo Sandoval <edsandoval.dev@gmail.com>
2025-12-22 21:59:06 -0600

    add decorator demo with extended constructor

--- src/topics/10-decorators.ts
+++ src/topics/10-decorators.ts
@@ -1,26 +1,30 @@
+/*
+Los decoradores permiten añadir o modificar comportamiento
+en clases, propiedades o métodos sin alterar directamente su código.
 
-function classDecorator<T extends { new (...args:any[]): {} }> (
-//function classDecorator (
-    //constructor : any
-    constructor : T,
-) {
+En este ejemplo, `classDecorator` recibe el constructor de una clase
+y retorna una nueva clase que extiende a la original, agregando
+propiedades adicionales.
+
+Esto hace posible “inyectar” lógica extra, como logging, validaciones,
+metadatos o comportamiento dinámico.
+
+📌 Nota: Los decoradores están basados en meta-programación y
+normalmente se usan en frameworks como Angular.
+*/
+function classDecorator<
+    T extends { new (...args: any[]): {} }
+>(constructor: T) {
     return class extends constructor {
         newProperty = "New Property";
         hello = "override";
-    }
+    };
 }
-
-/*
-- Los decoradores se pueden agregar a Clases, propiedades y métodos
-- Los decoradores añaden cierto comportamiento
-*/
 
 @classDecorator
 export class SuperClass {
 
-
-    public myProperty: string = "Abc123"
-
+    public myProperty: string = "Abc123";
 
     print() {
         console.log("Hola mundo");
